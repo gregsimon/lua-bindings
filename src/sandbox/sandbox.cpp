39d43de4951c86d3bf83ce57de38828afd5b25a5
Greg Simon <gregsimon@gmail.com>
2019-06-12 11:32:14 -0700

    run a lua program from argv1

--- src/sandbox/sandbox.cpp
+++ src/sandbox/sandbox.cpp
@@ -1,8 +1,20 @@
 // Testing Lua APIs
 
+#ifdef __cplusplus
+  #include "lua.hpp"
+#else
+  #include "lua.h"
+  #include "lualib.h"
+  #include "lauxlib.h"
+#endif
 
 
 int main(int argc, char** argv)
 {
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+
+    luaL_dofile(L, argv[1]);
+
     return 0;
 }
