d5dc9d0e8cf2c9c2d23412ea7c822f6e590f7fc8
Greg Simon <gregsimon@gmail.com>
2019-06-12 19:45:05 -0700

    capturing error codes

--- src/sandbox/sandbox.cpp
+++ src/sandbox/sandbox.cpp
@@ -1,20 +1,37 @@
-// Testing Lua APIs
 
-#ifdef __cplusplus
-  #include "lua.hpp"
-#else
+#include <iostream>
+
+extern "C" {
   #include "lua.h"
   #include "lualib.h"
   #include "lauxlib.h"
-#endif
+}
 
+extern "C" int error_cb(lua_State* L)
+{
+  int idx = lua_gettop(L);
+  size_t len;
+
+  std::cerr << "error - " << lua_tolstring(L, idx, &len) << std::endl;
+  return 1;
+}
 
 int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <luafile>" << std::endl;
+        return -10;
+    }
+
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
 
-    luaL_dofile(L, argv[1]);
+    lua_atpanic(L, error_cb);
+
+    luaL_loadfile(L, argv[1]);
+    lua_call(L, 0, LUA_MULTRET);
+
+    std::cerr << "end\n";
 
     return 0;
 }
